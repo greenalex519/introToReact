7eec860f83b10c2360938ec532b3281c5a84c64f
greenalex519 <32032234+greenalex519@users.noreply.github.com>
2018-06-21 16:55:40 +1000

    dummy code that successfully lists

--- src/App.js
+++ src/App.js
@@ -1,33 +1,70 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
-
 class App extends Component {
-  render() {
-    let myProject = [{name: "n1", summary: "d1"}, {name: "n2", summary: "s2"}, {name: "n3", summary: "s3"}];
-    let projectName = "";
-    let projectDesc = "";
-
-    for (let i=0; i < myProject.length; i++){
-       projectName = myProject[i].name
-       projectDesc = myProject[i].summary
-
-
-       console.log(projectName);
-       console.log(projectDesc);
-
+  constructor(props) {
+    super(props)
+    this.state = {
+    	items: [
+      	{ name: "Learn JavaScript", done: false },
+        { name: "Learn React", done: false },
+        { name: "Play around in JSFiddle", done: true },
+        { name: "Build something awesome", done: true }
+      ]
     }
+  }
 
+  render() {
     return (
       <div>
-        <h1>My Projects</h1>
-        <h3>{projectName}</h3>
-        <p>{projectDesc}</p>
+        <h2>Todos:</h2>
+        <ol>
+        {this.state.items.map(item => (
+          <li key={item.id}>
+            <label>
+              // <input type="checkbox" disabled readOnly checked={item.done} />
+              <span className={item.done ? "done" : ""}>{item.name}</span>
+            </label>
+          </li>
+        ))}
+        </ol>
       </div>
-    );
+    )
   }
 }
+
+
+
+
+
+
+
+
+// class App extends Component {
+//   render() {
+//     let myProject = [{name: "n1", summary: "d1"}, {name: "n2", summary: "s2"}, {name: "n3", summary: "s3"}];
+//     let projectName = "";
+//     let projectDesc = "";
+//
+//     for (let i=0; i < myProject.length; i++){
+//        projectName = myProject[i].name
+//        projectDesc = myProject[i].summary
+//
+//
+//        console.log(projectName);
+//        console.log(projectDesc);
+//
+//     }
+//
+//     return (
+//       <div>
+//         <h1>My Projects</h1>
+//         <h3>{projectName}</h3>
+//         <p>{projectDesc}</p>
+//       </div>
+//     );
+//   }
+// }
 
 
 
